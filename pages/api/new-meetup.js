e0d35a20659715b2752a048f9c677ac1e5efa2a4
Alexey Stashatov <alex008145@gmail.com>
2024-03-13 17:56:24 +0300

    Initial commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://Alex:cnfifnjd@cluster0.ohvnvhb.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
